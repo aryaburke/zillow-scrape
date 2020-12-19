8877043243b8f99ca50beb8168792ed90770eb0e
arya burke <aryaburke4@gmail.com>
2020-12-19 14:26:31 -0500

    edited to add a separate scrape function

--- handler.py
+++ handler.py
@@ -8,6 +8,20 @@ def hello(event, context):
     #this function runs on deploy
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": "Deployment successful!"
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+
+def scrape(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": zillow.main()
     }
 
